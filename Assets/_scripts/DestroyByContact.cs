a7dd264faee558bed1edad7e6461c915dd3f7b58
from-nibly <thatoneemail@gmail.com>
2015-02-26 20:10:53 -0700

    Initial Commit

---
+++ Assets/_scripts/DestroyByContact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+  public GameObject explosion;
+  public GameObject playerExplosion;
+
+  void OnTriggerEnter(Collider other) {
+    if (other.tag == "Boundary") {
+      return;
+    }
+    Destroy(other.gameObject);
+    Destroy(gameObject);
+    GameObject ex = Instantiate(explosion, transform.position, transform.rotation) as GameObject;
+    ex.rigidbody.angularVelocity = rigidbody.angularVelocity;
+    ex.rigidbody.velocity = rigidbody.velocity;
+    if (other.tag == "Player") {
+      GameObject pEx = Instantiate(playerExplosion, other.transform.position, other.transform.rotation) as GameObject;
+      pEx.rigidbody.angularVelocity = -1 * rigidbody.angularVelocity;
+    }
+  }
+
+}
---
+++ Assets/_scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+  public GameObject hazard;
+
+  void Start() {
+    SpawnWaves();
+  }
+
+  void SpawnWaves() {
+    Instantiate(hazard);
+  }
+}
